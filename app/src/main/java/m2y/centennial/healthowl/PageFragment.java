e3006c0f9d1df28902b9b286ebf656ec201ee2c9
MargoT <mtereshc@gmail.com>
2016-11-03 12:39:18 -0400

    Add sliding tabs activity and drawables

---
+++ app/src/main/java/m2y/centennial/healthowl/PageFragment.java
@@ -0,0 +1,50 @@
+package m2y.centennial.healthowl;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class PageFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private int mPage;
+
+    public static PageFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, page);
+        PageFragment fragment = new PageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PARAM1);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + mPage);
+        return view;
+    }
+
+
+
+
+}
