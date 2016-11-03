abb491a43f695a178694f82a160dabe5124a6e9e
MargoT <mtereshc@gmail.com>
2016-11-02 20:04:48 -0400

    Add navigation for login and menu

--- app/src/main/java/m2y/centennial/healthowl/menu.java
+++ app/src/main/java/m2y/centennial/healthowl/menu.java
@@ -1,13 +1,37 @@
 package m2y.centennial.healthowl;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class menu extends AppCompatActivity {
+
+    private Button mPatient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mPatient = (Button) findViewById(R.id.patientsButton);
+        mPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String testChoice = "test";
+                goPatientList(testChoice);
+
+            }
+        });
+
     }
+
+    private void goPatientList(String testChoice){
+        Intent intent = new Intent(this, patientMain.class);
+        intent.putExtra("testChoice", testChoice);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/m2y/centennial/healthowl/patientMain.java
+++ app/src/main/java/m2y/centennial/healthowl/patientMain.java
@@ -3,11 +3,11 @@ package m2y.centennial.healthowl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Patient extends AppCompatActivity {
+public class patientMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patient);
+        setContentView(R.layout.activity_patient_main);
     }
 }
